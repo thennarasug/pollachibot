6313d782caee4c5b54cf5e7f1aeb6cdef203756f
s0uthk1ng <51071207+s0uthk1ng@users.noreply.github.com>
2021-01-22 15:11:37 +0900

    Update pollachibot.py
    formatting

--- pollachibot.py
+++ pollachibot.py
@@ -1,21 +1,12 @@
-# Twython twitter api
 import time
-
-
-import glob
-import os
 import sqlite3
 import traceback
-import pandas as pd
+import datetime as dt
 
-# TelegramBot
-import telepot
-
+# Twython twitter api
 from twython import Twython, TwythonError
 #heroku env variables
 from os import environ
-
-import time
 
 def create_table():
     c = db.cursor()
@@ -43,7 +34,6 @@ def insert_error_string(id):
 def select_error_string(id):
     try:
         query = "select count(*) from pollachi where id = '{}'".format(id)
-        #data = pd.read_sql(query, con=db)
         cursor = db.execute(query)
         db.commit()
         return cursor
@@ -57,7 +47,8 @@ dbfilewithpath = ".\\pollachi.db"
 db = sqlite3.connect(dbfilewithpath)
 create_table()
 
-keywords =  ['pollachi','பொள்ளாச்சி']
+# heroku env variables
+keywords = ['pollachi', 'பொள்ளாச்சி']
 
 APP_KEY = environ.get('APP_KEY')
 APP_SECRET = environ.get('APP_SECRET')
@@ -66,23 +57,24 @@ OAUTH_TOKEN_SECRET = environ.get('OAUTH_TOKEN_SECRET')
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-#indefinite while loop that runs every 1 hour. To remove the dependency on scheduler.
+# indefinite while loop that runs every 1 hour. To remove the dependency on scheduler.
 while True:
-    count=0
-    trycount=0
-    totalcount=0
+    count = 0
+    trycount = 0
+    totalcount = 0
     for keyword in keywords:
-        for lang_list in ['en','ta']:
-            for result_type in ['popular','mixed', 'recent']:
+        for lang_list in ['en', 'ta']:
+            for result_type in ['popular', 'mixed', 'recent']:
                 try:
-                    #count=100 max (default 15), result_type='mixed' or 'recent'
-                    #count=15 max (default 15), result_type='popular'
-                    search_results = twitter.search(q=keyword,count=100, lang=lang_list, result_type=result_type)
+                    # count=100 max (default 15), result_type='mixed' or 'recent'
+                    # count=15 max (default 15), result_type='popular'
+                    search_results = twitter.search(q=keyword, count=100, lang=lang_list, result_type=result_type)
                 except TwythonError as e:
                     print(e)
 
-                 # print(search_results)
+                # print(search_results)
                 if len(search_results) > 0:
+                    count = 0
                     for tweet in search_results['statuses']:
                         utf8Pollachi = keyword.lower().encode('utf-8')
                         totalcount = totalcount + 1
@@ -92,23 +84,28 @@ while True:
                                 cursor = select_error_string(tweet['id'])
                                 for row in cursor:
                                     if row[0] == 0:
-                                        trycount = trycount + 1
                                         twitter.retweet(id=int(tweet['id']))
-                                        count = count + 1
                                         print(tweet['user']['screen_name'], "-->", tweet['id'], "--->", keyword.lower(), "--->",
                                               tweet['text'].lower())
                                         insert_error_string(tweet['id'])
-
+                                        count = count + 1
+                                        trycount = trycount + 1
+                                        totalcount = totalcount + 1
                             except TwythonError as e:
+                                trycount = trycount + 1
+                                totalcount = totalcount + 1
                                 if "You have already retweeted this Tweet" in str(e):
                                     insert_error_string(tweet['id'])
-                                print(e)
+                                else:
+                                    print(e)
+                        else:
+                            trycount = trycount + 1
+                            totalcount = totalcount + 1
 
-                    print(keyword, " in ", lang_list, ". Total filtered and retweeted ---> ", str(count), " / ", str(trycount),
-                          " / ", str(totalcount))
-    print("end of search")
-    print("sleeping for 3 hour")
-    time.sleep(3600)
+                    # print(keyword, "in", lang_list, "of", result_type, ". Total filtered and retweeted ---> ", str(count), " / ", str(trycount), " / ", str(totalcount))
+    #print("end of search")
+    print(dt.datetime.now(), "sleeping for 5mins")
+    time.sleep(360)
 else:
     db.close()
     print("***terminated***")
