39ba60e8a3c986325174a3b5c78a13fc864f6d6e
s0uthk1ng <51071207+s0uthk1ng@users.noreply.github.com>
2021-04-29 13:00:43 +0900

    added logger.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # pollachibot
 https://twitter.com/pollachibot
 
-This will retweet the tweets with specific keywords, like pollachi in ta and en languages. This is just used for gathering and information use only. one stop account for Pollachi in twitter.
+This will retweet the tweets with specific keywordskeywords ['pollachi', 'பொள்ளாச்சி', 'pollachidistrict', 'பொள்ளாச்சிமாவட்டம்'] in ta and en 
+languages. This is just used for gathering and information use only. one stop account for Pollachi in twitter.
--- pollachibot.py
+++ pollachibot.py
@@ -2,11 +2,15 @@ import datetime as dt
 import sqlite3
 import time
 import traceback
+import logging
+import Logger as log
 
 # Twython twitter api
 from twython import Twython, TwythonError
 #heroku env variables
 from os import environ
+
+log.basicconfig("pollachibot", level=logging.INFO)
 
 def create_table():
     c = db.cursor()
@@ -42,13 +46,14 @@ def select_error_string(id):
         db.rollback()
         return None
 
+
 # for database
 dbfilewithpath = "./pollachi.db"
 db = sqlite3.connect(dbfilewithpath)
 create_table()
 
 # heroku env variables
-keywords = ['pollachi', 'பொள்ளாச்சி']
+keywords = ['pollachi', 'பொள்ளாச்சி', 'pollachidistrict', 'பொள்ளாச்சிமாவட்டம்']
 
 APP_KEY = environ.get('APP_KEY')
 APP_SECRET = environ.get('APP_SECRET')
@@ -69,51 +74,64 @@ while True:
                     # count=100 max (default 15), result_type='mixed' or 'recent'
                     # count=15 max (default 15), result_type='popular'
                     search_results = twitter.search(q=keyword, count=100, lang=lang_list, result_type=result_type)
-                except TwythonError as e:
-                    print("error on search", e)
 
-                # print(search_results)
-                if len(search_results) > 0:
-                    count = 0
-                    for tweet in search_results['statuses']:
-                        utf8Pollachi = keyword.lower().encode('utf-8')
-                        totalcount = totalcount + 1
-                        a = tweet['text'].lower().count("pollachi")
-                        b = tweet['text'].lower().count("@pollachi")
-                        c = tweet['text'].lower().count("_pollachi")
-                        e = tweet['text'].lower().count("பொள்ளாச்சி")
-                        d = tweet['user']['screen_name'].lower().count("pollachi")
+                    # print(search_results)
+                    if len(search_results) > 0:
+                        count = 0
+                        for tweet in search_results['statuses']:
 
-                        # and utf8Pollachi in tweet['text'].lower().encode('utf-8')
-                        if (a + b + c + d == 0 or a + e > b + c) and tweet['user']['screen_name'].lower() not in ['signal_pollachi', 'pollachibot']:
-                            try:
-                                cursor = select_error_string(tweet['id'])
-                                for row in cursor:
-                                    if row[0] == 0:
-                                        twitter.retweet(id=int(tweet['id']))
-                                        print(tweet['user']['screen_name'], "-->", tweet['id'], "--->", keyword.lower(), "--->", tweet['text'].lower())
+                            utf8Pollachi = keyword.lower().encode('utf-8')
+                            totalcount = totalcount + 1
+                            a = tweet['text'].lower().count("pollachi")
+                            b = tweet['text'].lower().count("@pollachi")
+                            c = tweet['text'].lower().count("_pollachi")
+                            e = tweet['text'].lower().count("பொள்ளாச்சி")
+                            d = tweet['user']['screen_name'].lower().count("pollachi")
+
+                            # and utf8Pollachi in tweet['text'].lower().encode('utf-8')
+                            #print(a + b + c + d, " == 0", a + e, " > ", b + c)
+                            if (a + b + c + d == 0 or a + e > b + c) and tweet['user']['screen_name'].lower() not in ['signal_pollachi', 'pollachibot']:
+                                try:
+                                    cursor = select_error_string(tweet['id'])
+                                    for row in cursor:
+                                        if row[0] == 0:
+                                            twitter.retweet(id=int(tweet['id']))
+                                            print(tweet['user']['screen_name'], "-->", tweet['id'], "--->", keyword.lower(), "--->", tweet['text'].lower())
+                                            log.loginfo(tweet['user']['screen_name'], "-->", tweet['id'], "--->", keyword.lower(), "--->", tweet['text'].lower())
+                                            insert_error_string(tweet['id'])
+                                            count = count + 1
+                                            trycount = trycount + 1
+                                            totalcount = totalcount + 1
+                                            time.sleep(5)
+                                        else:
+                                            #print("already retweeted", tweet['id'], row[0])
+                                            pass
+                                except TwythonError as e:
+                                    trycount = trycount + 1
+                                    totalcount = totalcount + 1
+                                    if "You have already retweeted this Tweet" in str(e):
                                         insert_error_string(tweet['id'])
-                                        count = count + 1
-                                        trycount = trycount + 1
-                                        totalcount = totalcount + 1
+                                        #print("already retweeted", tweet['id'])
                                         time.sleep(5)
-                            except TwythonError as e:
+                                    elif "blocked" in str(e):
+                                        insert_error_string(tweet['id'])
+                                        print("error on action ****blocked**** !!!", tweet['user']['screen_name'], tweet['id'], tweet['text'], e)
+                                        log.logerror("error on action ****blocked**** !!!", tweet['user']['screen_name'], tweet['id'], tweet['text'], e)
+                                    else:
+                                        log.logerror("error on action!!!", e)
+                                        print("error on action!!!", e)
+                            else:
                                 trycount = trycount + 1
                                 totalcount = totalcount + 1
-                                if "You have already retweeted this Tweet" in str(e):
-                                    insert_error_string(tweet['id'])
-                                    print("error on action", tweet['id'], e)
-                                    time.sleep(5)
-                                else:
-                                    print("error on action", e)
-                        else:
-                            trycount = trycount + 1
-                            totalcount = totalcount + 1
 
-                    # print(keyword, "in", lang_list, "of", result_type, ". Total filtered and retweeted ---> ", str(count), " / ", str(trycount), " / ", str(totalcount))
+                        # print(keyword, "in", lang_list, "of", result_type, ". Total filtered and retweeted ---> ", str(count), " / ", str(trycount), " / ", str(totalcount))
+                except TwythonError as e:
+                    print("error on search", e)
+                    log.logerror("error on search", e)
             # added this to avoid "Twitter API returned a 429 (Too Many Requests), Rate limit exceeded"
             time.sleep(5)
     # print("end of search")
-    print(dt.datetime.now(), "sleeping for 30 mins")
+    log.loginfo("*****************************sleeping for 30min*****************************")
+    print(dt.datetime.now(), "*****************************sleeping for 30 mins*****************************")
     time.sleep(60 * 30)
 
